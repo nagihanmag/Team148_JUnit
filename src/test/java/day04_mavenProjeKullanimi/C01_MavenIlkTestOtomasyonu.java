58760199206c15172e6d11b6c89ff2faa7dae22e
nagihanmag <nagihan.kartal@outlook.com>
2024-12-19 14:14:03 +0300

    day04

---
+++ src/test/java/day04_mavenProjeKullanimi/C01_MavenIlkTestOtomasyonu.java
@@ -0,0 +1,49 @@
+package day04_mavenProjeKullanimi;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C01_MavenIlkTestOtomasyonu {
+    public static void main(String[] args) {
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        // Test otomasyonu ana sayfaya gidin
+        driver.get("https://www.testotomasyonu.com");
+
+
+
+        // phone icin arama yapin
+
+        WebElement aramaKutusu =driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("phone"+ Keys.ENTER);
+
+
+
+
+        // arama sonucunda urun bulunabildigini test edin
+
+        List<WebElement> bulunanElementleriList =driver.findElements(By.className("prod-img"));
+
+
+
+
+
+        //sayfayi kapatin
+        ReusableMethods.bekle(3);
+        driver.quit();
+
+
+    }
+}
