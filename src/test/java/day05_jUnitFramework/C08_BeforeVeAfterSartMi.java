4e74d593f60b65d0976e61f9678684445e8b9685
nagihanmag <nagihan.kartal@outlook.com>
2024-12-25 13:03:38 +0300

    day05

--- src/test/java/day05_jUnitFramework/C08_BeforeVeAfterSartMi.java
+++ src/test/java/day05_jUnitFramework/C08_BeforeVeAfterSartMi.java
@@ -1,8 +1,8 @@
 package day05_jUnitFramework;
 
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -14,8 +14,9 @@ import utilities.ReusableMethods;
 import java.time.Duration;
 import java.util.List;
 
-public class C07_JUnitAssertions {
-    // 3 farkli test method'u olusturarak asagidaki testleri gerceklestirin
+public class C08_BeforeVeAfterSartMi {
+
+    // tek test method'u olusturarak asagidaki testleri gerceklestirin
     // 1- Test otomasyonu anasayfaya gidin
     //    Url'in test otomasyonu icerdigini test edin
     // 2- phone icin arama yapin
@@ -23,25 +24,39 @@ public class C07_JUnitAssertions {
     // 3- ilk urunu tiklayin
     //    ve acilan sayfadaki urun isminde case sensitive olmadan "phone" bulundugunu test edin
 
+    WebDriver driver;
 
+    /*
+        Verilen gorev tek bir test method'u ile yapilacak bir gorev olsa da
 
-    static WebDriver driver;
+        Webdriver'i olusturma ve kapatma islemini ayri bir setup() ve teardown() ile
+        yapmayi tercih ederiz
 
-    @BeforeAll
-    static void setup(){
+        Eger tek bir test method'unun icinde
+        Webdriver olusturma ve sonunda driver'i kapatma islemlerini yaparsak
+        Test failed oldugunda exception olustugu icin
+        kodun calismasi durur ve son satirdaki driver.quit() calismaz
+
+        ozellikle toplu calistirmalarda
+        kapanmayan browser'larin olmasi
+        guzel olmaz
+
+     */
+
+    @BeforeEach
+    public void setup(){
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @AfterAll
-    static void teardown(){
+    @AfterEach
+    public void teardown(){
         driver.quit();
     }
 
     @Test
-    public void test01(){
-        // anasayfaTesti
+    public void urunTesti(){
         // 1- Test otomasyonu anasayfaya gidin
 
         driver.get("https://www.testotomasyonu.com");
@@ -53,11 +68,7 @@ public class C07_JUnitAssertions {
         String actualUrl = driver.getCurrentUrl();
 
         Assertions.assertTrue(  actualUrl.contains(expectedUrlIcerik) ,"Url beklendigi gibi degil" );
-    }
 
-    @Test
-    public void test02(){
-        //urunAramaTesti
         // 2- phone icin arama yapin
 
         WebElement aramaKutusu = driver.findElement(By.id("global-search"));
@@ -72,11 +83,6 @@ public class C07_JUnitAssertions {
 
         Assertions.assertTrue(actualBulunanUrunSayisi > 0,"Arama sonucunda urun bulunamadi");
 
-    }
-
-    @Test
-    public void test03(){
-        //ilkUrunIsimTesti
         // 3- ilk urunu tiklayin
 
         driver.findElement(By.xpath("(//*[@*='prod-img'])[1]"))
@@ -93,4 +99,5 @@ public class C07_JUnitAssertions {
         Assertions.assertTrue(actualUrunIsmi.contains(expectedIsimIcerik),"Urun ismi phone icermiyor");
 
     }
+
 }
