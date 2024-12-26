046f5856c3bac723d89dc370c1ae223742e228d7
nagihanmag <nagihan.kartal@outlook.com>
2024-12-26 11:54:41 +0300

    day06

--- src/test/java/day06_assertions_dropdowvMenu/C05_TestBaseKullanimi.java
+++ src/test/java/day06_assertions_dropdowvMenu/C05_TestBaseKullanimi.java
@@ -1,4 +1,31 @@
 package day06_assertions_dropdowvMenu;
 
-public class C05_TestBaseKullanimi {
+import org.junit.jupiter.api.Test;
+import utilities.ReusableMethods;
+import utilities.TestBaseEach;
+
+public class C05_TestBaseKullanimi extends TestBaseEach {
+
+     /*
+        Java'da OOP consept'in en buyuk avantaji REUSABILITY'dir
+
+        @BeforeEach - @AfterEach
+        @BeforeAll - @AfterAll
+        method'lari her class'da ayni sekilde yeniden yazmak yerine
+        baska bir class'da olusturabiliriz
+
+        Baska bir class'da bulunan class uyelerine
+        ulasmanin en kisa yolu inheritance'dir
+
+        biz de utilities altinda TestBase class'i olusturup
+        before ve after method'larini o class'lara koyabiliriz
+
+     */
+
+    @Test
+    public void test01(){
+        driver.get("https://www.testotomasyonu.com");
+        ReusableMethods.bekle(2);
+    }
+
 }
