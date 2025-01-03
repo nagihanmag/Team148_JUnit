d9305187736a116ee9e6b89ea4bcadc3ad867508
nagihanmag <nagihan.kartal@outlook.com>
2025-01-03 14:48:42 +0300

    day08

--- src/test/java/day08_windowSwitch_actionsClass/C09_KeybordActions.java
+++ src/test/java/day08_windowSwitch_actionsClass/C09_KeybordActions.java
@@ -1,6 +1,11 @@
 package day08_windowSwitch_actionsClass;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.ReusableMethods;
 import utilities.TestBaseEach;
 
 public class C09_KeybordActions extends TestBaseEach {
@@ -9,9 +14,26 @@ public class C09_KeybordActions extends TestBaseEach {
     public void test01(){
 
         //1- https://www.testotomasyonu.com sayfasina gidelim
+        driver.get("https://www.testotomasyonu.com ");
 
         //2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin
         //   ve Enter’a basarak arama yaptirin
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+
+        Actions actions = new Actions(driver);
+        ReusableMethods.bekle(1);
+
+        actions.click(aramaKutusu)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("dell c")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("ore ")
+                .keyDown(Keys.SHIFT)
+                .sendKeys("i")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("3")
+                .sendKeys(Keys.ENTER).perform();
+
 
         //3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
 
